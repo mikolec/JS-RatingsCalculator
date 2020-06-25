961961f4b8f6ad7ccabe8c6a685f6b100b76dc28
Mikolaj Lechtanski <mikolaj.lechtanski@capgemini.com>
2020-06-25 12:51:44 +0200

    Calculate Average Rating - fix

--- js/ratings.js
+++ js/ratings.js
@@ -6,11 +6,11 @@ function collect_ratings() {
   }
   let rating = 0;
   
-  const elements = document.querySelectorAll("rating");
+  const elements = document.querySelectorAll(".rating");
   elements.forEach(element => {
     rating = parseInt(element.id.replace("star", ""));
-    ratings.count = parseInt(element.value);
-    ratings.sum = parseInt(element.value) * rating;
+    ratings.count += parseInt(element.value);
+    ratings.sum += parseInt(element.value) * rating;
   });
   
   if(ratings.count != 0) {
@@ -22,5 +22,5 @@ function collect_ratings() {
 
 document.addEventListener("change", () => {
   const ratings = collect_ratings();
-  document.querySelector("#average").value = ratings.average;
+  document.querySelector("#average").value = ratings.average.toFixed(2);
 });
